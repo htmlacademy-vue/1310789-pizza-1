f1af93c3c7fd24c043cf4e88581fff75c270226a
Uladzimir Barysenka <vovaborisenko@live.com>
2021-07-11 19:07:44 +0300

    добавлены компонент "карточки"

---
+++ src/frontend/src/components/AppSheet.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="sheet">
+    <div class="sheet__title">
+      <slot name="title">
+        <AppTitle v-html="title" />
+      </slot>
+    </div>
+    <div class="sheet__content"><slot /></div>
+  </div>
+</template>
+
+<script>
+import AppTitle from "@/components/AppTitle";
+export default {
+  name: "AppSheet",
+  components: { AppTitle },
+  props: {
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sheet {
+  padding-top: 15px;
+
+  border-radius: 8px;
+  background-color: $white;
+  box-shadow: $shadow-light;
+}
+
+.sheet__title {
+  padding-right: 18px;
+  padding-left: 18px;
+}
+
+.sheet__content {
+  display: block;
+
+  margin-top: 8px;
+  padding-top: 18px;
+  padding-right: 18px;
+  padding-left: 18px;
+
+  border-top: 1px solid rgba($green-500, 0.1);
+}
+</style>
