e797efa55a191216f8cf9d8ecc5973b18b4a6fc2
Uladzimir Barysenka <vovaborisenko@live.com>
2021-07-11 19:03:13 +0300

    компонент логотипа

---
+++ src/frontend/src/components/AppLogo.vue
@@ -0,0 +1,32 @@
+<template>
+  <a href="/" class="logo">
+    <img :src="src" alt="V!U!E! Pizza logo" width="90" height="40" />
+  </a>
+</template>
+
+<script>
+import LogoSvg from "@/assets/img/logo.svg";
+
+export default {
+  name: "AppLogo",
+  props: {
+    src: {
+      type: String,
+      default: LogoSvg,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  display: block;
+
+  img {
+    display: block;
+
+    width: 90px;
+    height: 40px;
+  }
+}
+</style>
