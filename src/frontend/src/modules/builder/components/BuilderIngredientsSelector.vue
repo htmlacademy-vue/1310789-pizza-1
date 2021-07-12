ed81547f59556652ed89498d08054e364c221935
Uladzimir Barysenka <vovaborisenko@live.com>
2021-07-12 20:51:35 +0300

    добавлены компоненты билдеры

---
+++ src/frontend/src/modules/builder/components/BuilderIngredientsSelector.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="ingredients">
+    <div v-if="sauces.length" class="ingredients__sauce">
+      <p>Основной соус:</p>
+
+      <RadioButton
+        v-for="sauce in sauces"
+        :key="sauce.value"
+        group-name="sauce"
+        :label="sauce.name"
+        :value="sauce.value"
+        :checked="checkedSauce"
+        class="ingredients__input"
+        @input="$emit('change-sauce', sauce.value)"
+      />
+    </div>
+
+    <div class="ingredients__filling">
+      <p>Начинка:</p>
+
+      <ul class="ingredients__list">
+        <li
+          v-for="filling in fillings"
+          :key="filling.value"
+          class="ingredients__item"
+        >
+          <AppDrag :draggable="filling.count < 3" :transfer-data="filling">
+            <SelectorItem :label="filling.name" :value="filling.value" />
+          </AppDrag>
+
+          <ItemCounter v-model="filling.count" class="ingredients__counter" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SelectorItem from "@/components/SelectorItem";
+import ItemCounter from "@/components/ItemCounter";
+import RadioButton from "@/components/RadioButton";
+import { SAUCE_DEFAULT } from "@/common/constants";
+import AppDrag from "@/components/AppDrag";
+
+export default {
+  name: "BuilderIngredientsSelector",
+  components: { AppDrag, RadioButton, ItemCounter, SelectorItem },
+  props: {
+    sauces: {
+      type: Array,
+    },
+    checkedSauce: {
+      type: String,
+      default: SAUCE_DEFAULT,
+    },
+    fillings: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.ingredients__sauce {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  width: 100%;
+  margin-bottom: 14px;
+
+  p {
+    @include r-s16-h19;
+
+    margin-top: 0;
+    margin-right: 16px;
+    margin-bottom: 10px;
+  }
+}
+
+.ingredients__input {
+  margin-right: 24px;
+  margin-bottom: 10px;
+}
+
+.ingredients__filling {
+  width: 100%;
+
+  p {
+    @include r-s16-h19;
+
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
+}
+
+.ingredients__list {
+  @include clear-list;
+
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+
+.ingredients__item {
+  width: 100px;
+  min-height: 40px;
+  margin-right: 17px;
+  margin-bottom: 35px;
+}
+
+.ingredients__counter {
+  width: 54px;
+  margin-top: 10px;
+  margin-left: 36px;
+}
+</style>
---
+++ src/frontend/src/modules/builder/components/BuilderPizzaView.vue
@@ -0,0 +1,180 @@
+<template>
+  <AppDrop @drop="$emit('drop', $event)">
+    <div class="pizza" :class="pizzaClassName">
+      <div class="pizza__wrapper">
+        <template v-for="filling in fillings">
+          <div
+            :key="filling.value"
+            :class="['pizza__filling', `pizza__filling--${filling.value}`]"
+          />
+          <div
+            v-if="filling.count > 1"
+            :key="`${filling.value}-second`"
+            :class="[
+              'pizza__filling',
+              'pizza__filling--second',
+              `pizza__filling--${filling.value}`,
+            ]"
+          />
+          <div
+            v-if="filling.count > 2"
+            :key="`${filling.value}-third`"
+            :class="[
+              'pizza__filling',
+              'pizza__filling--third',
+              `pizza__filling--${filling.value}`,
+            ]"
+          />
+        </template>
+      </div>
+    </div>
+  </AppDrop>
+</template>
+
+<script>
+import AppDrop from "@/components/AppDrop";
+export default {
+  name: "BuilderPizzaView",
+  components: { AppDrop },
+  props: {
+    dough: {
+      type: String,
+      validator: (value) => ["light", "large"].indexOf(value) !== -1,
+    },
+    sauce: {
+      type: String,
+      validator: (value) => ["tomato", "creamy"].indexOf(value) !== -1,
+    },
+    fillings: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  computed: {
+    pizzaClassName() {
+      const doughSize = { light: "small", large: "big" }[this.dough];
+
+      return `pizza--foundation--${doughSize}-${this.sauce}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.pizza {
+  position: relative;
+
+  display: block;
+
+  box-sizing: border-box;
+  width: 100%;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+
+  &--foundation--big-creamy {
+    background-image: url("~@/assets/img/foundation/big-creamy.svg");
+  }
+
+  &--foundation--big-tomato {
+    background-image: url("~@/assets/img/foundation/big-tomato.svg");
+  }
+
+  &--foundation--small-creamy {
+    background-image: url("~@/assets/img/foundation/small-creamy.svg");
+  }
+
+  &--foundation--small-tomato {
+    background-image: url("~@/assets/img/foundation/small-tomato.svg");
+  }
+}
+
+.pizza__wrapper {
+  width: 100%;
+  padding-bottom: 100%;
+}
+
+.pizza__filling {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  display: block;
+
+  width: 100%;
+  height: 100%;
+
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+
+  &--second {
+    transform: rotate(45deg);
+  }
+
+  &--third {
+    transform: rotate(-45deg);
+  }
+
+  &--ananas {
+    background-image: url("~@/assets/img/filling-big/ananas.svg");
+  }
+
+  &--bacon {
+    background-image: url("~@/assets/img/filling-big/bacon.svg");
+  }
+
+  &--blue_cheese {
+    background-image: url("~@/assets/img/filling-big/blue_cheese.svg");
+  }
+
+  &--cheddar {
+    background-image: url("~@/assets/img/filling-big/cheddar.svg");
+  }
+
+  &--chile {
+    background-image: url("~@/assets/img/filling-big/chile.svg");
+  }
+
+  &--ham {
+    background-image: url("~@/assets/img/filling-big/ham.svg");
+  }
+
+  &--jalapeno {
+    background-image: url("~@/assets/img/filling-big/jalapeno.svg");
+  }
+
+  &--mozzarella {
+    background-image: url("~@/assets/img/filling-big/mozzarella.svg");
+  }
+
+  &--mushrooms {
+    background-image: url("~@/assets/img/filling-big/mushrooms.svg");
+  }
+
+  &--olives {
+    background-image: url("~@/assets/img/filling-big/olives.svg");
+  }
+
+  &--onion {
+    background-image: url("~@/assets/img/filling-big/onion.svg");
+  }
+
+  &--parmesan {
+    background-image: url("~@/assets/img/filling-big/parmesan.svg");
+  }
+
+  &--salami {
+    background-image: url("~@/assets/img/filling-big/salami.svg");
+  }
+
+  &--salmon {
+    background-image: url("~@/assets/img/filling-big/salmon.svg");
+  }
+
+  &--tomatoes {
+    background-image: url("~@/assets/img/filling-big/tomatoes.svg");
+  }
+}
+</style>
