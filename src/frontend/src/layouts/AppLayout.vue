8135f3107212022aab1de3ba13e65ae67419fdd2
Uladzimir Barysenka <vovaborisenko@live.com>
2021-07-12 20:52:06 +0300

    собраны компоненты

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-layout">
+    <AppLayoutHeader />
+    <Index />
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+import Index from "@/views/Index.vue";
+
+export default {
+  name: "AppLayout",
+  components: { Index, AppLayoutHeader },
+};
+</script>
+
+<style></style>
