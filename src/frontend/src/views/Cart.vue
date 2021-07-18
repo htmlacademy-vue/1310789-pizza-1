0a65f7ca401e9e112f2690dd0209336e52d2429b
Uladzimir Barysenka <vovaborisenko@live.com>
2021-07-18 11:44:02 +0300

    подключение маршрутизатора; создание автоматического шаблона

---
+++ src/frontend/src/views/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppTitle>Cart</AppTitle>
+</template>
+
+<script>
+import AppTitle from "@/common/components/AppTitle";
+
+/**
+ * Контент страницы "Корзина"
+ */
+export default {
+  name: "Cart",
+  components: { AppTitle },
+};
+</script>
+
+<style></style>
---
+++ src/frontend/src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppTitle>Profile</AppTitle>
+</template>
+
+<script>
+import AppTitle from "@/common/components/AppTitle";
+
+/**
+ * Контент страницы "UserProfile"
+ */
+export default {
+  name: "Profile",
+  components: { AppTitle },
+};
+</script>
+
+<style></style>
